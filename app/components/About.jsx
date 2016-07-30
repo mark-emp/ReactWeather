cb03cbb9b454059189aacb2ac3e6e98520bb6416
Mark Patton <mark@emp-technical.com>
2016-07-30 18:49:52 +0100

    Custom styling for app

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>This is a weather application that has been built using the <a href="https://facebook.github.io/react" target="_blank">React</a> JavaScript library as part of <a href="https://www.udemy.com/the-complete-react-web-app-developer-course" target="_blank">The Complete React Web App Developer Course</a>.</p>
       <p>Weather information is taken from <a href="https://openweathermap.org" target="_blank">OpenWeatherMap</a>.</p>
       <p>The GitHub repository containing the soucrce code can be found <a href="https://github.com/mark-emp/ReactWeather" target="_blank">here</a>.</p>
