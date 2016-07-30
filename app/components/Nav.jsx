27ebbf0cc0e80895369ce999fafb71e1442a137a
Mark Patton <mark@emp-technical.com>
2016-07-30 15:37:01 +0100

    Style the Weather form and message

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -22,7 +22,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li><input type="search" placeholder="Search Weather"/></li>
-              <li><input type="submit" className="button" value="Get Weather"/></li>
+              <li><input type="submit" className="button hollow" value="Get Weather"/></li>
             </ul>
           </form>
         </div>
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp,location}) => {
   return(
-      <p>It is {temp} in {location}!</p>
+      <h3 className="text-center">It is {Math.round(temp)}&#8451; in {location}!</h3>
   );
 }
 
